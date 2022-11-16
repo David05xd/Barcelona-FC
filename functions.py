0875160ff0260b9cd26886cc03245974da42a995
varadi david <varadi.david@students.jedlik.eu>
2022-11-16 10:40:52 +0100

    asd

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-from main import age
+# from main import age
 
 def menu():
     print('Választható funkciók: \n 1---Hány éve szerepelnek \n 2---Legeredményesebb szezon \n 3---Leggyengébb szezon \n 4---Legtöbb lőtt gól a szezonban \n 5---Eddigi Top 3 szezonok \n 6---Szezonlekérés')
@@ -8,5 +8,3 @@ def menu():
     else:
         return False
 
-def eletkor():
-    pass
