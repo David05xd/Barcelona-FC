8be753e71570a21189ce3e8ac57b9da76c92db4f
varadi david <varadi.david@students.jedlik.eu>
2022-11-17 09:00:34 +0100

    login fix

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-from functions import MainMenu, TortenelemMenu, SzezonbeliMeccsek, Jegyvasarlas, Jegyeim, OsszesSzereples
+from functions import MainMenu, TortenelemMenu, SzezonbeliMeccsek, Jegyvasarlas, Jegyeim, OsszesSzereples, signup, login, main_login_signup
 from fileHandling import loadData
 
 
 print('Üdvözöljük az FC Barcelona hivatalos oldalán!')
 
-
-valasz = -1
-while valasz != 5:
-    valasz = int(MainMenu())
-    if valasz == 1:
-        if TortenelemMenu() == 1:
-            print(f'Eddig {OsszesSzereples()}-szer szerepeltek a bajnokságba.')
-    elif valasz == 2:
-        SzezonbeliMeccsek()
-    elif valasz == 3:
-        Jegyvasarlas()
-    elif valasz == 4:
-        Jegyeim()
+main_login_signup()
 
 
